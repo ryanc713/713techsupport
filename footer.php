8a75598f7b3e5703335b3581b353d057553f31f8
Ryan Cook <71239722+ryanc713@users.noreply.github.com>
2021-02-26 17:51:03 -0600

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,11 @@
+<head>
+<!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Oxygen&display=swap" rel="stylesheet">
+<!-- Custom CSS -->
+    <link rel="stylesheet" href="index-style.css" type="text/css">
+</head>
+<div class="footer">
+    <a href="https://www.facebook.com/713houstonIT"><img src="images/facebook.png" width="64px" height="64px"></a>
+    <a href="<img src="https://g.page/713techsupport?we">images/google.png" width="64px" height="64px"></a>
+</div>
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+include('header.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!- Meta Tags for SEO --------------->
+    <meta name=”description” content=”Here is a precise description of my awesome webpage.”>
+
+<!- Google Fonts -------------------->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Oxygen&display=swap" rel="stylesheet">
+
+<!- Custom CSS ---------------------->
+    <link rel="stylesheet" href="index-style.css" type="text/css">
+
+<title>713TechSupport.com</title>
+</head>
+<body>
+
+</body>
+</html>
