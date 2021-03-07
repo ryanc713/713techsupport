710d1d1f6572a9974fdb6d658a13aab464afac4e
Ryan Cook <71239722+ryanc713@users.noreply.github.com>
2021-03-07 14:36:01 -0600

    Create guide_ubuntu.php

---
+++ guide_ubuntu.php
@@ -0,0 +1,40 @@
+<style>
+a {
+  text-decoration: none;
+  color: #00A0FF;
+  transition: 0.3s;
+}
+a:hover {
+  text-decoration: underline;
+  color: #0048FF;
+}
+</style>
+<?php
+include('navbar.php');
+?>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!- Meta Tags for SEO --------------->
+<meta name="description" content="713TechSupport.com - Tutorials and How Tos for Ubuntu Server">
+
+<!- GOOGLE FONTS --------------------->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Recursive:wght@300;400;700&display=swap" rel="stylesheet">
+
+<!- W3 CSS ---------------->
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+</head>
+<body>
+  <div class="container">
+    <div class="col-10"></div>
+
+        <div class="col-80">
+<center><a href="guide_passwordless-login.php"><h3>Passwordless Login using SSH</h3></a></center>
+<center><a href="guide_apache-vhosts.php"><h3>Configuring Virtual Hosts with Apache</h3></a></center>
+        </div>
+    <div class="col-10"></div>
+  </div>
+</body>
