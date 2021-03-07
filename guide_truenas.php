8d43b6d316b7503b7ca94d10fa1817c211367e51
Ryan Cook <71239722+ryanc713@users.noreply.github.com>
2021-03-07 14:35:24 -0600

    Create guide_truenas.php

---
+++ guide_truenas.php
@@ -0,0 +1,15 @@
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!- Meta Tags for SEO --------------->
+<meta name="description" content="Heres the description">
+
+<!- GOOGLE FONTS --------------------->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Recursive:wght@300;400;700&display=swap" rel="stylesheet">
+
+<!- W3 CSS ---------------->
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+</head>
