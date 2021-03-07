aecc7790c5fa70176ad302c2f0f608c97e2582ff
Ryan Cook <71239722+ryanc713@users.noreply.github.com>
2021-03-07 14:34:58 -0600

    Create guide_rpi.php

---
+++ guide_rpi.php
@@ -0,0 +1,40 @@
+<?php
+include('navbar.php');
+?>
+<style>
+.guide-link {
+  text-decoration: none;
+  color: #00A0FF;
+  transition: 0.3s;
+}
+.guide-link:hover {
+  text-decoration: underline;
+  color: #0048FF;
+}
+</style>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!- Meta Tags for SEO --------------->
+<meta name="description" content="713TechSupport.com - List of Raspberry Pi Tutorials and How-Tos">
+
+<!- GOOGLE FONTS --------------------->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Recursive:wght@300;400;700&display=swap" rel="stylesheet">
+
+<!- W3 CSS ---------------->
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+</head>
+<body>
+  <div class="container">
+    <div class="col-10"></div>
+    <div class="col-80">
+<center><h1>Raspberry Pi Tutorials and How-To's</h1></center>
+<br>
+<center><a class="guide-link" href="guide_kali-rpi.php"><h3>Installing Kali Linux on Raspberry Pi</h3></a></center>
+<center><a class="guide-link" href=""><h3>Install MotionEyeOS on Raspberry Pi</h3></a></center>
+    </div>
+    <div class="col-10"></div>
+</div>
