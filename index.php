688bffd9384b3ef0fded55d2c43551a3ed6586db
Ryan Cook <71239722+ryanc713@users.noreply.github.com>
2021-03-07 14:32:19 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,54 @@
 <?php
-include('header.php');
+include('navbar.php');
 ?>
-<!DOCTYPE html>
-<html>
+<style>
+.container {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+}
+.col-10 {
+  width: 10%;
+}
+.col-80 {
+  width: 80%;
+  padding: 5px 10px;
+  margin-top: 20px;
+  background-color: #00FF84;
+  color: #000000;
+  border: 2px solid #ffffff;
+  border-radius: 10px;
+}
+</style>
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <!- Meta Tags for SEO --------------->
-    <meta name=”description” content=”Here is a precise description of my awesome webpage.”>
+<meta name="description" content="Heres the description">
 
-<!- Google Fonts -------------------->
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Oxygen&display=swap" rel="stylesheet">
+<!- GOOGLE FONTS --------------------->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Recursive:wght@300;400;700&display=swap" rel="stylesheet">
 
-<!- Custom CSS ---------------------->
-    <link rel="stylesheet" href="index-style.css" type="text/css">
+<!- W3 CSS ---------------->
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 
-<title>713TechSupport.com</title>
 </head>
-<body>
 
+<body>
+  <div class="container">
+<div class="col-10"></div>
+        <div class="col-80">
+<center><h2>About 713techsupport.com..</h2></center>
+<center><p>We are a Houston local IT services company. Our services range from Network configurations, to Security Systems</p></center>
+<center><p>Installation. We do just about everything and anything that you can plug into a wall (and some things that don't!).</p></center>
+<center><p>Our goal is to provide the same, if not better, quality you would find at a major company such as "Geek Squad", but</p></center>
+<center><p>for a fraction of the cost. Estimates are always free and if you're not satisified with our work, you will not be</p></center>
+<center><p>charged. That is how confident we are about the work that we do! For more information on some of the services</p></center>
+<center><p>we provide, you can head over to the Services page by clicking HERE, or on the Services link at the top of this page.</p></center>
+<center><h3>Thank you for visiting 713techsupport.com, and we look forward to serving you! Stay safe Houston.</h3></center>
+        </div>
+<div class="col-10"></div>
 </body>
-</html>
